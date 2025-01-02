747ee592638239db0624d2033903d916e6328ff4
ntsirintanis <ntsirintanis@therp.nl>
2025-01-02 15:42:47 +0100

    [IMP] product_pricelist_item_sequence: pre-commit stuff

--- junglegym_product/models/product_pricelist_item.py
+++ junglegym_product/models/product_pricelist_item.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 
 class ProductPricelistItem(models.Model):
     _inherit = "product.pricelist.item"
-    # NOTE: we do not change _order, we ar jest adding sequence 
+    # NOTE: we do not change _order, we ar jest adding sequence
     # and visualizing a different order in view.
 
     ppi_sequence = fields.Integer(default=16)
